5c48798fc8ab887aea86229b137a74f14a535560
Edward Prokopik <ejprok@gmail.com>
2020-03-07 15:18:45 -0800

    Create example project

---
+++ example/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:slate/slate.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Slate Demo',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Slate(),
+    );
+  }
+}
--- lib/slate.dart
+++ lib/slate.dart
@@ -1,7 +1,10 @@
 library slate;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class Slate extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text("slate");
+  }
 }
