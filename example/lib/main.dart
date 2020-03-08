dd1f507eadfd69758defab695ab705e76bf1a010
Edward Prokopik <ejprok@gmail.com>
2020-03-07 16:51:26 -0800

    Create basic Slate widget with limited functionality

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -18,8 +18,42 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Slate(),
+      backgroundColor: Colors.blue,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Center(
+            child: Slate(
+              child: Container(
+                width: 100,
+                height: 100,
+                child: Center(
+                  child: Icon(Icons.play_arrow, size: 50),
+                ),
+              ),
+              color: Colors.blue,
+              rounding: 30,
+            ),
+          ),
+          Center(
+            child: Card(
+              elevation: 6,
+              clipBehavior: Clip.antiAlias,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: Container(
+                width: 100,
+                height: 100,
+                child: Center(
+                  child: Icon(Icons.play_arrow, size: 50),
+                ),
+              ),
+              color: Colors.blue,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/slate.dart
+++ lib/slate.dart
@@ -3,8 +3,35 @@ library slate;
 import 'package:flutter/material.dart';
 
 class Slate extends StatelessWidget {
+  final Widget child;
+  final Color color;
+  final double rounding;
+  Slate({@required this.child, @required this.color, this.rounding})
+      : assert(child != null);
+
   @override
   Widget build(BuildContext context) {
-    return Text("slate");
+    const Color lowerShadow = Colors.black26;
+    const Color upperShadow = Color.fromARGB(30, 255, 255, 255);
+
+    return Container(
+      decoration: BoxDecoration(
+        color: this.color,
+        borderRadius: BorderRadius.all(Radius.circular(this.rounding)),
+        boxShadow: [
+          BoxShadow(
+              color: lowerShadow,
+              offset: Offset(3.0, 3.0),
+              blurRadius: 5.0,
+              spreadRadius: 1.0),
+          BoxShadow(
+              color: upperShadow,
+              offset: Offset(-3.0, -3.0),
+              blurRadius: 5.0,
+              spreadRadius: 1.0),
+        ],
+      ),
+      child: this.child,
+    );
   }
 }
